2d9cb7dbdc5ef3e7603e0a81761931254425229e
Jack Lawson <ajacksified@gmail.com>
2016-03-09 08:41:11 -0800

    Fix some loose ends around token management

--- routes.es6.js
+++ routes.es6.js
@@ -5,12 +5,20 @@ export default function routes (app) {
   const { router } = app;
 
   router.use(async (ctx, next) => {
+    // Has to be a let, or the compiler barfs when creating a Buffer
     let tokenString = ctx.cookies.get('token');
 
     if (tokenString) {
-      const b = new Buffer(tokenString, 'base64').toString('ascii');
-      const t = JSON.parse(b);
-      ctx.props.token = t;
+      try {
+        const b = new Buffer(tokenString, 'base64').toString('ascii');
+        const t = JSON.parse(b);
+        ctx.props.token = t;
+      } catch (e) {
+        // there was an error decoding the token, but it was probably already
+        // handled upstream by the server. Annoyingly, it doesn't change the
+        // value in `cookies.get` when you use `cookies.set`, so we will just
+        // catch it here.
+      }
     }
 
     return await next();
